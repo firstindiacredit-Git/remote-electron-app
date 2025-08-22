f8f8cbcbfb38e554330cc3fc080d91a9364e4383
PizeonFly <pizeonflyn@gmail.com>
2025-08-22 12:36:28 +0530

    Update robotjsControl.js

--- robotjsControl.js
+++ robotjsControl.js
@@ -20,7 +20,7 @@ function createWindow() {
 
   // Connect to the socket.io server
   // const socket = io("http://15.206.194.12:8080"); // Change to your server IP if needed
-  const socket = io("http://192.168.29.140:8080"); // Change to your server IP if needed
+  const socket = io("http://192.168.29.140:8080", "http://localhost:5173"); // Change to your server IP if needed
 
   // Handle connection
   socket.on("connect", () => {
